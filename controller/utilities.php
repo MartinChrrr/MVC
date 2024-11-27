163f245470e22bf9b26705ad5bb4a8441174fcb5
MartinChrrr <martin_charrier@yahoo.fr>
2024-11-27 07:50:25 +0100

    ajout de la fonction pour appeler les boutons

--- controller/utilities.php
+++ controller/utilities.php
@@ -4,3 +4,39 @@ function DrawPage(array $data) {
     require $view;
 }
 
+//Test if 
+//@parameters 
+function CheckArray(string $name, string $word, array $array) {
+    switch($name){
+        case "tags":
+            if(in_array($word, $array)) {
+                DrawTagsButton($word, true);
+            } else {
+                DrawTagsButton($word);
+            }
+            break;
+        case "games":
+            if(in_array($word, $array)) {
+                DrawGamesButton($word, true);
+            } else {
+                DrawGamesButton($word);
+            }
+            break;
+        case "horaire":
+            if(in_array($word, $array)) {
+                DrawHoraireButton($word, true);
+            } else {
+                DrawHoraireButton($word);
+            }
+            break;
+        case "gender":
+            if(in_array($word, $array)) {
+                DrawGenderOption($word, true);
+            } else {
+                DrawGenderOption($word);
+            }
+            break;
+        default:
+            break;
+    }
+}
